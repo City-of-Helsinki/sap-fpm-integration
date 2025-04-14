cddd5f28cb6351df3727856d91f315a60c09b91f
Ilkka Kujanpää <ilkka.kujanpaa@devikone.com>
2025-04-14 16:58:19 +0300

    try SAS

--- src/main/java/fi/hel/integration/sapfpm/routes/sapkumppani/PartInRouteBuilder.java
+++ src/main/java/fi/hel/integration/sapfpm/routes/sapkumppani/PartInRouteBuilder.java
@@ -55,7 +55,8 @@ public class PartInRouteBuilder extends PerustiedotRouteBuilder {
             .to("direct:part-csv-out")
             .choice()
                 .when(simple("${exchangeProperty.outDir} == 'palke'"))
-                    .setProperty("uploadFileDir", constant("SAS/TEST"))
+                  //  .setProperty("uploadFileDir", constant("SAS/TEST"))
+                .setProperty("uploadFileDir", constant("SAS"))
                     .to("direct:upload-blob-to-azure-" + toimiala)
             .end();
     }

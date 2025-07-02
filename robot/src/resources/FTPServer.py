a6c90e8056aa373e87a76e8aa8675ed6258a018a
Ilkka Kujanpää <ilkka.kujanpaa@devikone.com>
2025-07-02 15:45:25 +0300

    robot: remove passive ports setting from ftp server

--- robot/src/resources/FTPServer.py
+++ robot/src/resources/FTPServer.py
@@ -19,7 +19,6 @@ class FTPServer(object):
         self.ftp_dir = os.path.join(os.getcwd(), relative_ftp_dir)
         if not os.path.exists(self.ftp_dir): os.makedirs(self.ftp_dir)
         handler = FTPHandler
-        handler.passive_ports = [2122, 2123]
         handler.masquerade_address = masquerade_address
         handler.authorizer = DummyAuthorizer()
         self.server = servers.FTPServer(address, handler)
